e5f8ee092b148c0e62c46d153bc4ecc33e99924c
wamunyima3 <51529595+wamunyima3@users.noreply.github.com>
2022-10-02 19:44:59 +0200

    Add files via upload

---
+++ Arch_installation_script.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/bash
+# This is the arch installation script
+# Written by wamunyima Mukelabai
+: '
+    This script is not for sale
+
+    Some commands in this script you can follow or reference to this video:
+    https://youtu.be/DPLnBPM4DhI
+
+    wiki article of the same commands
+    https://www.learnlinux.tv/arch-linux-full-installation-guide/
+
+    if you loved the scrip you can subscribe to my channel
+    https://www.youtube.com/playlist?list=PLuR0XcKiKJXJAhFWNsZdTP3RYQgcvC6yg
+
+
+    it will encourage me to advance it or even write more scripts to automate different defficult stuff in linux
+'
+select option in nonEFI EFI EFIincription
+    do
+        case $option in
+            nonEFI)
+                echo "nonEFI selected";;
+            EFI)
+                echo "EFI selected";;
+            EFIincription)
+                echo "EFI selected";;
+            *)
+                echo "Inalid choice";;
+        esac
+    done
+    
